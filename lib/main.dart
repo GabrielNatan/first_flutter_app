41f8fa8426fa123907d0be7a24f127b09dbd1403
gabriel natan <g.souza@grouplinknetwork.com>
2022-10-12 17:15:26 -0300

    Add Rows and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,65 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: Stack(
-        alignment: AlignmentDirectional.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.red,
-            height: 100,
-            width: 100,
-            ),
-          Container(
-            color: Colors.green,
-            height: 50,
-            width: 50,
-            ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.green,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.red,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.cyan,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.pink,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.purple,
+                height: 50,
+                width: 50,
+              )
+            ],
+          )
         ],
       ),
     );
   }
 }
-
