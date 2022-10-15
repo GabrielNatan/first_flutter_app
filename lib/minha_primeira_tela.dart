4aa4b437781895f94dca5895d4bbe5c557a4fa23
gabriel natan <g.souza@grouplinknetwork.com>
2022-10-15 17:54:01 -0300

    change my first app

--- lib/minha_primeira_tela.dart
+++ lib/minha_primeira_tela.dart
@@ -1,74 +1,85 @@
-// Container(
-//           color: Colors.white,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               Stack(
-//                 alignment: AlignmentDirectional.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.red,
-//                     height: 100,
-//                     width: 100,
-//                   ),
-//                   Container(
-//                     color: Colors.green,
-//                     height: 50,
-//                     width: 50,
-//                   ),
-//                 ],
-//               ),
-//               Stack(
-//                 alignment: AlignmentDirectional.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.green,
-//                     height: 100,
-//                     width: 100,
-//                   ),
-//                   Container(
-//                     color: Colors.red,
-//                     height: 50,
-//                     width: 50,
-//                   ),
-//                 ],
-//               ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Container(
-//                     color: Colors.cyan,
-//                     height: 50,
-//                     width: 50,
-//                   ),
-//                   Container(
-//                     color: Colors.pink,
-//                     height: 50,
-//                     width: 50,
-//                   ),
-//                   Container(
-//                     color: Colors.purple,
-//                     height: 50,
-//                     width: 50,
-//                   )
-//                 ],
-//               ),
-//               Container(
-//                 color: Colors.amber,
-//                 width: 300,
-//                 height: 30,
-//                 child: Text(
-//                   'Diamant Amarelo',
-//                   style: TextStyle(
-//                     color: Colors.black,
-//                     fontSize: 28,
-//                     decoration: TextDecoration.none
-//                   ),
-//                   textAlign: TextAlign.center,
-//                 ),
-//               ),
-//               ElevatedButton(onPressed: (){print('Gabriel');}, child: Text("Aperte o botão!"),)
-//             ],
-//           ),
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.green,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Container(
+                    color: Colors.red,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.purple,
+                    height: 50,
+                    width: 50,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                width: 300,
+                height: 30,
+                child: Text(
+                  'Diamant Amarelo',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    decoration: TextDecoration.none
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ElevatedButton(onPressed: (){print('Gabriel');}, child: Text("Aperte o botão!"),)
+            ],
+          ));}
+}
